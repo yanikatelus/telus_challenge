c64d31c38e4a0905eb0aa1440e425dac8d386216
Yanika Telus <yanikatelus@gmail.com>
2021-12-22 02:23:18 -0500

    created

---
+++ index.js
@@ -0,0 +1,14 @@
+// document.getElementById('click_more').style.width = "0px"
+
+function more(){
+    const svg = document.getElementById('exampleModal').attributes;
+
+    if (svg['aria-hidden'].value === "true"){
+        document.getElementById('explore_svg').setAttribute("aria-hidden", "false");
+        document.getElementById('explore_svg').style.opacity = 0;
+        } else {
+        document.getElementById('explore_svg').setAttribute("aria-hidden", "true");
+        document.getElementById('explore_svg').style.opacity = 1;
+    }
+
+}
